7742a5840dec89a6f763230d1a8a952d77243e06
Yorik van Havre <yorik@uncreated.net>
2014-03-16 16:23:19 -0300

    First changes

---
+++ js/search.js
@@ -0,0 +1,17 @@
+//console.log('search.js initialized');
+
+var searchform = document.querySelector("#search");
+
+if (searchform) {
+  searchform.onsubmit = function () {
+    var searchfield = document.querySelector("#searchfield");
+    if (searchfield) {
+      searchterm = searchfield.value;
+      searchterm = searchterm.replace(/s/g,"%20");
+      searchurl = "https://duckduckgo.com/?q="+searchterm 
+      console.log("search form submitted:"+searchurl);
+      new MozActivity({ name: "view", data: { type: "url", url: searchurl} });
+    }
+    return false;
+  };
+}
